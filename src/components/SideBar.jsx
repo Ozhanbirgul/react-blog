c0ec3ea7a07cebe8f7c01783003fa0d6849fd2c6
Birgül Özhan <birgul@clicktopeak.com>
2025-04-30 10:09:19 +0300

    fix

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,7 +8,7 @@ function Sidebar() {
     useEffect(() => {
        fetch("http://localhost:5001/blogs")
        .then(res => res.json())
-       .then(data => setPopularBlogs(data.slice(0,15)))
+       .then(data => setPopularBlogs(data?.slice(0,15)))
     }, [])
   return (
     <div>
@@ -16,8 +16,8 @@ function Sidebar() {
         <h3 className='text-2xl font-semibold px-4'>Letest Blogs</h3>
         <div>
             {
-                popularBlogs.slice(0,5).map(blog => <div key={blog.id} className='my-5 border-b-2 border-spacing-2 px-4'>
-                    <h4 className='font-medium mb-2'>{blog.title}</h4>
+                popularBlogs.slice(0,5).map(blog => <div key={blog?.id} className='my-5 border-b-2 border-spacing-2 px-4'>
+                    <h4 className='font-medium mb-2'>{blog?.title}</h4>
                     <Link to="/" className='text-base pb-2 font-semibold hover:text-orange-500 inline-flex items-center py-1'>
                         Read More <FaArrowRight className='mt-1 ml-2' />
                     </Link>
@@ -33,8 +33,8 @@ function Sidebar() {
         <h3 className='text-2xl font-semibold px-4 mt-20'>Popular Blogs</h3>
         <div>
             {
-                popularBlogs.slice(5,11).map(blog => <div key={blog.id} className='my-5 border-b-2 border-spacing-2 px-4'>
-                    <h4 className='font-medium mb-2'>{blog.title}</h4>
+                popularBlogs.slice(5,11)?.map(blog => <div key={blog?.id} className='my-5 border-b-2 border-spacing-2 px-4'>
+                    <h4 className='font-medium mb-2'>{blog?.title}</h4>
                     <Link to="/" className='text-base pb-2 font-semibold hover:text-orange-500 inline-flex items-center py-1'>
                         Read More <FaArrowRight className='mt-1 ml-2' />
                     </Link>
